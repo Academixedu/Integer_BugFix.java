38df07e87f666b21e491826f02ba8e753b592f40
kavitha-guntru <kavithaguntru60@gmail.com>
2024-07-31 17:40:28 +0530

    kavitha

--- Bug1.java
+++ Bug1.java
@@ -1,19 +1,30 @@
+import java.util.Scanner;
 public class Bug1{
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	System.out.println("Enter your Number");
-	
+
+	int temp=0;
 	int a=in.nextInt();
-	for(int i=1;i<a;i++) {
+	for(int i=1;i<a;i++) 
+	{
 		if(a%i==0) {
 			a=a+i;
+			temp=temp+i;
 			System.out.println(i);
 		}
-		
+
 	}
 	if(i==a) {
-		System.out.println("Perfect Number"+a);
+		// Fix the Bug and Print Perfect Number or Not
+		if(temp==a) {
+			System.out.println("Perfect Number"+temp);
+        }
+		else {
+			System.out.println("not a perfecrt");
+        }
 	}
-// Fix the Bug and Print Perfect Number or Not
 }
 }
+
+
--- Bug3.java
+++ Bug3.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
 public class Bug3{
-public static void main(String[] args) {
+public class Palindrome{
+public static void main(String[] args) 
+{
 	Scanner in=new Scanner(System.in);
 System.out.println("Enter");
-	int a=in.nextInt();
+	int	a=in.nextInt();
 	int temp=0;
 	int pal=a;
 while(a!=0) {
-	int rem=a/10;
-	temp=temp/10+rem;
-	a=a%10;
+	int rem=a%10;
+	temp=temp*10+rem;
+	a=a/10;
 }
 if(pal==temp) {
 System.out.println("Palindrome------"+temp);	
@@ -16,6 +19,8 @@ System.out.println("Palindrome------"+temp);
 else 
 {
 System.out.println("Reverse----"+temp);
-	// Fix the Issue and Check Palindrome or not
-}	}
+// Fix the Issue and Check Palindrome or not
+}	
+}
+}
 }
