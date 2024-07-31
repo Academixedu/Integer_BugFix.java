f75d8b67529520d8b0c3b12d68b893ffd8cd8c53
Bharathmanne18 <bharathmanne12@gmail.com>
2024-07-31 18:31:54 +0530

    Update Bug4.java

--- Bug4.java
+++ Bug4.java
@@ -1,8 +1,9 @@
 public class Bug4{
+  public static void main(string[]args){
 Scanner in=new Scanner(System.in);
 int temp=1;
 int a=in.nextInt();
-for(int i=1;i!=a;i--) {
+for(int i=1;i<a;i++) {
 temp=temp*i;	
   // Fix the Bugs and Issues
 }
