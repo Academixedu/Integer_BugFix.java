29e247af070742f6efec5bbb0513becca0f151ab
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-31 16:16:58 +0530

    bugs

--- Bug4.java
+++ Bug4.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class Bug4{
+  public static void main(String[] args) {
+    
+  
 Scanner in=new Scanner(System.in);
 int temp=1;
+System.out.println("enter a");
+
 int a=in.nextInt();
-for(int i=1;i!=a;i--) {
+for(int i=1;i<=a;i++) {
 temp=temp*i;	
   // Fix the Bugs and Issues
 }
