df4e2c48f4180e0b12836638d83f47ab5418923a
goutham120203 <gouthampaladugu12@gmail.com>
2024-08-02 22:45:18 +0530

    goutham

--- Bug1.java
+++ Bug1.java
@@ -1,18 +1,23 @@
+import java.util.*;
 public class Bug1{
 public static void main(String[] args) {
+	int temp = 0;
 	Scanner in=new Scanner(System.in);
 	System.out.println("Enter your Number");
 	
 	int a=in.nextInt();
 	for(int i=1;i<a;i++) {
 		if(a%i==0) {
-			a=a+i;
+			temp = temp+i;
 			System.out.println(i);
 		}
 		
 	}
-	if(i==a) {
+	if(a == temp ) {
 		System.out.println("Perfect Number"+a);
+	}
+	else{
+		System.out.println("not a perfect number");
 	}
 // Fix the Bug and Print Perfect Number or Not
 }
