1f14d447794c585af60195c4801b3c9c98e8097d
sheik-noorahammed <your@nsyed6348@gmail.com>
2024-09-05 19:51:55 +0530

    save

--- Bug3.java
+++ Bug3.java
@@ -1,21 +1,26 @@
-public class Bug3{
-public static void main(String[] args) {
-	Scanner in=new Scanner(System.in);
-System.out.println("Enter");
-	int a=in.nextInt();
-	int temp=0;
-	int pal=a;
-while(a!=0) {
-	int rem=a/10;
-	temp=temp/10+rem;
-	a=a%10;
-}
-if(pal==temp) {
-System.out.println("Palindrome------"+temp);	
-}
-else 
-{
-System.out.println("Reverse----"+temp);
-	// Fix the Issue and Check Palindrome or not
-}	}
+import java.util.Scanner;
+
+public class Bug3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int a = in.nextInt();  
+        
+        int original = a;  
+        int reversed = 0;  
+        
+        while (a != 0) {
+            int rem = a % 10;  
+            reversed = reversed * 10 + rem;  
+            a = a / 10;  
+        }
+        
+        if (original == reversed) {
+            System.out.println("Palindrome: " + reversed);
+        } else {
+            System.out.println("Reverse: " + reversed);
+        }
+        
+        in.close();  
+    }
 }
