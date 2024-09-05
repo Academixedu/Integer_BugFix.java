3fb1eb90f0f47349091fdde5d899f81ee36839c2
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-09-05 18:28:43 +0530

    bug2

--- Bug2.java
+++ Bug2.java
@@ -1,28 +1,34 @@
+import java.util.Scanner;
+
 public class Bug2{
-public class Fibonacci {
-public void scan() {
-	int a,b,c,d;	
-	Scanner in=new Scanner(System.in);
-	System.out.println("Give Your Number");
-	a=in.nextInt();
-	System.out.println("Give Sequential Number");
-	b=in.nextInt();
-	System.out.println("Give Your Count");
-	d=in.nextInt();
-	System.out.println("First Number"+a);
-	System.out.println("Second Number"+b);
-for(int e=0;e<d;e++) {
-d=a+b;
-System.out.print(c+",");
-c=b;
-a=c;
+	public class Fibonacci {
+	public void scan() {
+		int a,b,c;	
+		Scanner in=new Scanner(System.in);
+		System.out.println("Give Your Number");
+		a=in.nextInt();
+		System.out.println("Give Sequential Number");
+		b=in.nextInt();
+		System.out.println("Give Your Count");
+		c=in.nextInt();
+		System.out.println("First Number"+a);
+		System.out.println("Second Number"+b);
+		System.out.println(a+""+b);
+	for(int e=2;e<c;e++) {
+	c=a+b;
+	System.out.print("," +c);
+	a=b;
+	b=c;
+	}
 }
-}	
-  // Fix the Bug Issues and Erros and Solve Them
-	public static void main(String[] args) {
-		Fibonacci f=new Fibonacci();
-f.scan();		
-
-
-}
-}
+	}
+	
+	  // Fix the Bug Issues and Erros and Solve Them
+		public static void main(String[] args) {
+			
+			Bug2.Fibonacci f=new Bug2().new Fibonacci();
+	f.scan();		
+	
+	
+	}
+	}
