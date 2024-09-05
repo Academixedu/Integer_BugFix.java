b175869c0aa9bd60cb50dc8702fe25b31861110e
Shubham-napit <ashoknapit7999@gmail.com>
2024-09-05 19:52:16 +0530

    done

--- Bug1.java
+++ Bug1.java
@@ -1,19 +1,26 @@
-public class Bug1{
-public static void main(String[] args) {
-	Scanner in=new Scanner(System.in);
-	System.out.println("Enter your Number");
-	
-	int a=in.nextInt();
-	for(int i=1;i<a;i++) {
-		if(a%i==0) {
-			a=a+i;
-			System.out.println(i);
-		}
-		
-	}
-	if(i==a) {
-		System.out.println("Perfect Number"+a);
-	}
-// Fix the Bug and Print Perfect Number or Not
-}
+import java.util.Scanner;
+
+public class Bug1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your Number");
+
+        int a = in.nextInt();  // The input number
+        int sum = 0;  // To store the sum of divisors
+
+        // Loop to find divisors of the number
+        for (int i = 1; i < a; i++) {
+            if (a % i == 0) {
+                sum += i;  // Add divisor to sum
+                System.out.println("Divisor: " + i);
+            }
+        }
+
+        // Check if the sum of divisors equals the original number
+        if (sum == a) {
+            System.out.println(a + " is a Perfect Number");
+        } else {
+            System.out.println(a + " is not a Perfect Number");
+        }
+    }
 }
--- Bug4.java
+++ Bug4.java
@@ -1,11 +1,19 @@
-public class Bug4{
-Scanner in=new Scanner(System.in);
-int temp=1;
-int a=in.nextInt();
-for(int i=1;i!=a;i--) {
-temp=temp*i;	
-  // Fix the Bugs and Issues
-}
-System.out.println("factorial:"+temp);
-}
+import java.util.Scanner;
+
+public class Bug4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter a number:");
+        int a = in.nextInt();
+        
+        int temp = 1;
+
+        // Corrected loop to calculate factorial
+        for (int i = 1; i <= a; i++) {
+            temp = temp * i;
+        }
+        
+        System.out.println("Factorial: " + temp);
+    }
 }
